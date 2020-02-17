45c241c14d12ceb191f421ea433c9cdef360306d
Hamel, Robert [ETHUS NON-J&J] <RHamel1@its.jnj.com>
2020-02-17 15:48:52 -0500

    init

---
+++ src/main/java/com/example/cassandratlspoc/CassandraTlsPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.cassandratlspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraTlsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraTlsPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cassandratlspoc/CassandraTlsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cassandratlspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassandraTlsPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
